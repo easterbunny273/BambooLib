3b5f480be80497dfb33b1fbcb2b99f1636aea8b1
Christian Möllinger <ch.moellinger@gmail.com>
2015-11-19 22:49:06 +0100

    removed all source files and added scopeguard header

---
+++ include/ScopeGuard.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#ifndef _BAMBOOLIB_SCOPEGUARD_H_
+#define _BAMBOOLIB_SCOPEGUARD_H_
+
+#include <functional>
+
+namespace BambooLib
+{
+	template <class T>
+	class ScopeGuard
+	{
+	public:
+		ScopeGuard(const T & func) : m_func(func), m_engaged(true) {};
+		~ScopeGuard() { if (m_engaged) m_func(); }
+
+		ScopeGuard(const ScopeGuard<T>&) = delete;
+		ScopeGuard<T>& operator=(const ScopeGuard<T> &) = delete;
+
+		void release() { m_engaged = false; }
+
+	private:
+		T m_func;
+		bool m_engaged;
+	};
+
+	template <class T> auto createScopeGuard(T & func)
+	{
+		return ScopeGuard<typename std::decay<T>::type>(func);
+	}
+}
+
+
+#endif
--- test/main.cpp
+++ test/main.cpp
@@ -1,9 +1,18 @@
 #include "gtest/gtest.h"
+#include "ScopeGuard.h"
 
 TEST(A, B) { ASSERT_EQ(2, 2); }
 
 int main(int argc, char **argv)
 {
+	{
+		auto scopeGuard = BambooLib::createScopeGuard([&]() { std::cout << "clean up";});
+
+		int a = 3;
+		int b = a + 2;
+	}
+	
+
     ::testing::InitGoogleTest(&argc, argv);
     int ret = RUN_ALL_TESTS();
     return ret;
